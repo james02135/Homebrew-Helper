1ccad7fbcc162baf5de84a55461493bc78eed0d1
james <james02135@hotmail.com>
2020-11-05 16:56:49 +0000

    Comments added, final UI/UX tweaks
    Signed-off-by: james <james02135@hotmail.com>

--- src/main/kotlin/org/wit/homebrew/controllers/HomebrewController.kt
+++ src/main/kotlin/org/wit/homebrew/controllers/HomebrewController.kt
@@ -1,19 +1,22 @@
 package org.wit.homebrew.controllers
 
 import mu.KotlinLogging
+import org.wit.homebrew.models.HomebrewMemStore
 import org.wit.homebrew.models.HomebrewJSONStore
 import org.wit.homebrew.models.HomebrewModel
 import org.wit.homebrew.views.HomebrewView
 
 class HomebrewController {
 
+    //val homebrews = HomebrewMemStore()
     val homebrews = HomebrewJSONStore()
     val homebrewView = HomebrewView()
     val logger = KotlinLogging.logger {}
 
     init {
-        logger.info { "Launching Homebrew Console App" }
+        //logger.info { "Launching Homebrew Console App" }
         println("Homebrew Kotlin App Version 4.0")
+        println()
     }
 
     fun start() {
@@ -27,30 +30,44 @@ class HomebrewController {
                 3 -> list()
                 4 -> search()
                 5 -> delete()
-                -1 -> println("Exiting App")
+                -1 -> println()
                 else -> println("Invalid Option")
             }
             println()
         } while (input != -1)
-        logger.info { "Shutting Down Homebrew Helper Console App" }
+        println("Shutting down the Homebrew Kotlin App...")
     }
 
     fun menu() :Int { return homebrewView.menu() }
 
+    /*
+    Takes the data entered by the user in HomebrewView.kt in the addHomebrew funtion
+    and adds it to the list of homebrews
+     */
+    
     fun add(){
         var aHomebrew = HomebrewModel()
         if (homebrewView.addHomebrewData(aHomebrew)) {
             homebrews.create(aHomebrew)
-            logger.info("Homebrew Added")
+            println("Homebrew has been added")
         }
         else {
-            logger.info("Homebrew Not Added")
+            println("Homebrew Not Added")
         }
     }
+
+    /*
+    Lists all the homebrews contained in the JSON file
+     */
 
     fun list() {
         homebrewView.listHomebrews(homebrews)
     }
+
+    /*
+    Updates the name and style of a homebrew after the
+    user has entered the ID of the homebrew
+     */
 
     fun update() {
         homebrewView.listHomebrews(homebrews)
@@ -60,14 +77,14 @@ class HomebrewController {
             if(homebrewView.updateHomebrewData(aHomebrew)) {
                 homebrews.update(aHomebrew)
                 homebrewView.showHomebrew(aHomebrew)
-                logger.info("Homebrew Updated : [ $aHomebrew ]")
             }
-            else
-                logger.info("Homebrew Not Updated")
         }
-        else
-            println("Homebrew Not Updated")
     }
+
+    /*
+    Deletes the homebrew from the list once the user
+    has entered the ID of the homebrew
+     */
 
     fun delete() {
         homebrewView.listHomebrews(homebrews)
@@ -75,18 +92,26 @@ class HomebrewController {
         var aHomebrew = search(searchId)
         if (aHomebrew != null) {
             homebrews.delete(aHomebrew)
-            println("Homebrew Deleted")
-            homebrewView.listHomebrews(homebrews)
+            println()
+            println("Homebrew has been deleted")
         }
         else
             println("Homebrew Not Deleted")
     }
+
+    /*
+    Helper function to search the list
+     */
 
     fun search() {
         homebrewView.listHomebrews(homebrews)
         val aHomebrew = search(homebrewView.getId())!!
         homebrewView.showHomebrew(aHomebrew)
     }
+
+    /*
+    searches the list for the entered ID
+     */
 
     fun search(id: Long) : HomebrewModel? {
         var foundHomebrew = homebrews.findOne(id)
